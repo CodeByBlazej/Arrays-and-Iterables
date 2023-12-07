4bcf2d654b327119dd6d5061054425efa0db45cf
CodeByBlazej <codebyblazej@gmail.com>
2023-12-07 22:12:53 +0000

    lesson

---
+++ app.js
@@ -0,0 +1,38 @@
+// const numbers = [1, 2, 3];
+// console.log(numbers);
+
+// // const moreNumbers = new Array(5);
+// // console.log(moreNumbers);
+
+// // const yetMoreNumbers = Array.of(1, 2);
+// // console.log(yetMoreNumbers);
+
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
+
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems);
+
+// const hobbies = ['Cooking', 'Sports'];
+// const personalData = [30, 'Blazej', {moreDetail: []}];
+
+// const analyticsData = [[1, 1.6], [-5.4, 2.1]];
+
+// for (const data of analyticsData) {
+//   for (const dataPoint of data) {
+//     console.log(dataPoint);
+//   }
+// }
+
+// console.log(personalData[1]);
+
+const hobbies = ['Sports', 'Cooking'];
+hobbies.push('Swimming');
+hobbies.unshift('Reading');
+hobbies.pop();
+hobbies.shift();
+console.log(hobbies);
+
+hobbies[1] = 'Coding';
+// hobbies[5] = 'Reading'; // rarely used
+console.log(hobbies, hobbies[4]);
