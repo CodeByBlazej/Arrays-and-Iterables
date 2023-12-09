e530aea0dd5bd3440528b4fb91bb175bcd4dc30c
CodeByBlazej <codebyblazej@gmail.com>
2023-12-09 20:19:04 +0000

    Update app.js

--- app.js
+++ app.js
@@ -43,29 +43,76 @@
 // const removedElements = hobbies.splice(-1, 1);
 // console.log(hobbies);
 
-const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
-// const storedResults = testResults.slice(2); 
-const storedResults = testResults.concat([3.99, 2]);
+// const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
+// // const storedResults = testResults.slice(2); 
+// const storedResults = testResults.concat([3.99, 2]);
 
-testResults.push(5.91);
+// testResults.push(5.91);
 
-console.log(storedResults, testResults);
-// console.log(testResults.indexOf(1.5));
-console.log(testResults.lastIndexOf(1.5));
+// console.log(storedResults, testResults);
+// // console.log(testResults.indexOf(1.5));
+// console.log(testResults.lastIndexOf(1.5));
 
-const personData = [{ name: 'Blazej' }, { name: 'Max' }];
-console.log(personData.indexOf({ name: 'Max' }));
+// console.log(testResults.includes(10.99));
+// console.log(testResults.indexOf(10.99) !== -1);
 
-const max = personData.find((person, idx, persons) => {
-  return  person.name === 'Max';
+// const personData = [{ name: 'Blazej' }, { name: 'Max' }];
+// console.log(personData.indexOf({ name: 'Max' }));
+
+// const max = personData.find((person, idx, persons) => {
+//   return  person.name === 'Max';
+// });
+
+// max.name = 'Anna';
+
+// console.log(max, personData);
+
+// const blazejIndex = personData.findIndex((person, idx, persons) => {
+//   return  person.name === 'Blazej';
+// });
+
+// console.log(blazejIndex);
+
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+// const taxAdjustedPrices = [];
+
+// // for (const price of prices) {
+// //   taxAdjustedPrices.push(price * (1 + tax));
+// // }
+
+// prices.forEach((price, idx, prices) => {
+//   const priceObj = {index: idx, taxAdjustedPrice: price * (1 + tax)};
+//   taxAdjustedPrices.push(priceObj);
+// });
+
+// console.log(taxAdjustedPrices);
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
+  const priceObj = {index: idx, taxAdjustedPrice: price * (1 + tax)};
+  return priceObj;
 });
 
-max.name = 'Anna';
+// console.log(prices, taxAdjustedPrices);
 
-console.log(max, personData);
-
-const blazejIndex = personData.findIndex((person, idx, persons) => {
-  return  person.name === 'Blazej';
+const sortedPrices = prices.sort((a, b) => {
+  if (a > b) {
+    return -1;
+  } else if (a === b) {
+    return 0;
+  } else {
+    return 1;
+  }
 });
 
-console.log(blazejIndex);
+// console.log(sortedPrices.reverse());
+console.log(sortedPrices);
+
+const filteredArray = prices.filter((price, index, prices) => {
+  return price > 6;
+});
+
+console.log(filteredArray);
